a3f44c94c96e9e4b5c255425c18e6c2959f1756a
Harpie <silvia.lesnaia@gmail.com>
2026-07-11 13:47:21 +0400

    Папка custom хранит мои настройки сервера, модулей, а так же мои скрипты и запросы

---
+++ custom/sql/Aedesra.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:99917bea4371e0daf3c82ad6a1814fe0df6d86a142491831f8506fc30a097bd5
+size 1319
---
+++ custom/sql/Sarotein.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3d8fca06893b34a5659e8754509040ebc70ec5a9a2014401fb9a19f4a0db8c4c
+size 1320
